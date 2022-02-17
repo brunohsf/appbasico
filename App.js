d3b51af817f46fcfb9721e1f3cbb15074ff4f38f
Bruno Henrique <brunohsfrancisco@gmail.com>
2022-02-17 20:03:20 -0300

    add new components

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
+import React, { Component } from 'react';
 import { View, Text, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={{ backgroundColor: 'red' }} >
-      <Text style={{ fontSize: 25, color: '#FFF', textAlign: 'center' }}>
-        appMuseum
-      </Text>
-      <Text>MUSEU DE JALES</Text>
-      <View>
-        <Image source={{ uri: 'https://i.imgur.com/mVvh7pG.png' }} style={{ width: 300, height: 300 }} />
-      </View>
-    </View>
-  );
+class App extends Component {
+  render() {
+    let cidade = 'JALES - SP';
 
+    return (
+      <View>
+        <Text>My App</Text>
+        <Text>Primeiro App com React Native</Text>
+        <Text style={{ color: 'red' }}>Exemplo de Props</Text>
+        <Text style={{ color: '#00FFF', fontSize: 25, margin: 15 }}>Exemplo de Props</Text>
+        <Image source={{ uri: 'https://i.imgur.com/mVvh7pG.png' }} style={{ width: 300, height: 300 }} />
+        <Text style={{ fontFamily: 'arial', fontStyle: 'italic', fontSize: 35 }}>{cidade}</Text>
+      </View>
+    );
+  }
 }
+
+export default App
