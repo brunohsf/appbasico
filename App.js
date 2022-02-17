bc2a29b62d1ca4761dbcd29df00c929b048b29aa
Bruno Henrique <brunohsfrancisco@gmail.com>
2022-02-17 20:28:22 -0300

    add class image

--- App.js
+++ App.js
@@ -11,9 +11,22 @@ class App extends Component {
         <Text>Primeiro App com React Native</Text>
         <Text style={{ color: 'red' }}>Exemplo de Props</Text>
         <Text style={{ color: '#00FFF', fontSize: 25, margin: 15 }}>Exemplo de Props</Text>
-        <Image source={{ uri: 'https://i.imgur.com/mVvh7pG.png' }} style={{ width: 300, height: 300 }} />
+        <Imagem largura={420} altura={400} />
         <Text style={{ fontFamily: 'arial', fontStyle: 'italic', fontSize: 35 }}>{cidade}</Text>
       </View>
+    );
+  }
+}
+
+class Imagem extends Component {
+  render() {
+    let img = 'https://i.imgur.com/mVvh7pG.png';
+
+    return (
+      <View>
+        <Image source={{ uri: { img } }} style={{ width: this.props.largura, height: this.props.altura }} />
+      </View>
+
     );
   }
 }
