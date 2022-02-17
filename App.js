fb642d45a61eb105b168ab4bd997880508003a4d
Bruno Henrique <brunohsfrancisco@gmail.com>
2022-02-16 22:23:54 -0300

    add title and maybe an image

---
+++ App.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ backgroundColor: 'red' }} >
+      <Text style={{ fontSize: 25, color: '#FFF', textAlign: 'center' }}>
+        appMuseum
+      </Text>
+      <Text>MUSEU DE JALES</Text>
+      <View>
+        <Image source={{ uri: 'https://i.imgur.com/mVvh7pG.png' }} style={{ width: 300, height: 300 }} />
+      </View>
+    </View>
+  );
+
+}
