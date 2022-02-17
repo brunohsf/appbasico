1a68e7316046397c2482018c2cc531446a863617
Bruno Henrique <56417608+brunohsf@users.noreply.github.com>
2022-02-17 20:31:13 -0300

    Aula/aula02 (#1)
    * add new components
    
    * add class image

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
+import React, { Component } from 'react';
 import { View, Text, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={{ backgroundColor: 'red' }} >
-      <Text style={{ fontSize: 25, color: '#FFF', textAlign: 'center' }}>
-        appMuseum
-      </Text>
-      <Text>MUSEU DE JALES</Text>
-      <View>
-        <Image source={{ uri: 'https://i.imgur.com/mVvh7pG.png' }} style={{ width: 300, height: 300 }} />
-      </View>
-    </View>
-  );
+class App extends Component {
+  render() {
+    let cidade = 'JALES - SP';
 
+    return (
+      <View>
+        <Text>My App</Text>
+        <Text>Primeiro App com React Native</Text>
+        <Text style={{ color: 'red' }}>Exemplo de Props</Text>
+        <Text style={{ color: '#00FFF', fontSize: 25, margin: 15 }}>Exemplo de Props</Text>
+        <Imagem largura={420} altura={400} />
+        <Text style={{ fontFamily: 'arial', fontStyle: 'italic', fontSize: 35 }}>{cidade}</Text>
+      </View>
+    );
+  }
 }
+
+class Imagem extends Component {
+  render() {
+    let img = 'https://i.imgur.com/mVvh7pG.png';
+
+    return (
+      <View>
+        <Image source={{ uri: { img } }} style={{ width: this.props.largura, height: this.props.altura }} />
+      </View>
+
+    );
+  }
+}
+
+export default App
