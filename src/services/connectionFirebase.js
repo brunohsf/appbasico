81da1b711d7d0823426e00a1db505bd92988b9c4
Bruno Henrique <brunohsfrancisco@gmail.com>
2022-03-24 20:37:42 -0300

    remove informacoes firebase

--- src/services/connectionFirebase.js
+++ src/services/connectionFirebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/database'
 
 let firebaseConfig = {
-  apiKey: "AIzaSyBChStaWtP7NQ2Jk5V8E6aUxSdIc89Fhww",
-  authDomain: "database-carlos-lanches.firebaseapp.com",
-  projectId: "database-carlos-lanches",
-  storageBucket: "database-carlos-lanches.appspot.com",
-  messagingSenderId: "675961307726",
-  appId: "1:675961307726:web:e46685cbb290c58ce1791f"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 if(!firebase.apps.length) {
